c4aa021909c803256bfbe0e19a61032865c0fd2c
aimless-cpu <s.grabher@protonmail.com>
2021-05-03 17:31:59 +0200

    finished class Customer

--- src/com/company/Customer.java
+++ src/com/company/Customer.java
@@ -1,4 +1,39 @@
 package com.company;
 
+import java.util.Random;
+
 public class Customer {
+    //instance
+    private String customerName;
+    private int customerNumber;
+
+    //constructor
+    public Customer(String customerName, int customerNumber) {
+        this.customerName = customerName;
+        this.customerNumber = createCustomerNumber();
+    }
+
+    //method
+    public int createCustomerNumber() {
+        Random rand = new Random();
+        int n = 100000 + rand.nextInt(900000);
+        return n;
+    }
+
+    //getter setter
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public int getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+    }
 }
